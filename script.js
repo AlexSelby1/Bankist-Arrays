b5d1cf4068b3a87391ab2d7bf3ad11c4e6a6f886
Alex Selby <alex.selby@hotmail.com>
2021-10-19 12:24:33 +0100

    adding dates array to accounts

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -12,6 +25,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -19,6 +45,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-04-08T15:12:59.604Z',
+    '2020-04-27T16:02:20.194Z',
+    '2020-06-11T23:35:17.929Z',
+    '2020-06-13T11:40:35.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'de-DE',
 };
 
 const account4 = {
@@ -26,6 +65,19 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    '2019-10-12T21:31:17.178Z',
+    '2019-11-22T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-01-29T10:17:24.185Z',
+    '2020-04-08T15:12:59.604Z',
+    '2020-04-27T16:02:20.194Z',
+    '2020-06-11T23:30:30.929Z',
+    '2020-06-23T11:40:35.790Z',
+  ],
+  currency: 'GBP',
+  locale: 'en-EN',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -130,7 +182,7 @@ btnLogin.addEventListener("click", function (e) {
     (acc) => acc.username === inputLoginUsername.value
   );
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +(inputLoginPin.value)) {
     //Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(" ")[0]
@@ -169,7 +221,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +(inputLoanAmount.value);
 
   if (
     amount > 0 &&
@@ -185,7 +237,7 @@ btnClose.addEventListener("click", function (e) {
   e.preventDefault();
   if (
     currentAccount.username === inputCloseUsername.value &&
-    currentAccount.pin === Number(inputClosePin.value)
+    currentAccount.pin === +(inputClosePin.value)
   ) {
     const index = accounts.findIndex(
       (acc) => acc.username === currentAccount.username
